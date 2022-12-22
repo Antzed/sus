6b845cddba24c99e61e857fd7bcf42cc92e3d747
antzed <anthoz3@uw.edu>
2022-12-21 23:12:45 -0800

    some whoppies left

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import Button from 'react-bootstrap/Button';
 import { SussyBakas } from './Components/sussy-bakas';
 import song from './song/youtube-grd-K33tOSM-Among Us Drip Theme Song Original (Among Us Trap Remix _ Amogus Meme Music).webm';
 
-
 function App() {
   function susInitial(){
-    return <SussyBakas className="sussyBaka" />
+    return <SussyBakas className="sussyBaka"/>
   }
 
   const [showButton, setShowButton] = React.useState("");
   const [showSussyBakas, setShowSussyBakas] = React.useState(null);
   const [overallClass, setOverallClass] = React.useState("startButton");
- 
+
   const startGame = () => {
     setShowButton("hide");
     setOverallClass("startButton sussy-background");
     setShowSussyBakas(susInitial);
-
-    
-
   }
 
   const audio = new Audio(song);
@@ -28,22 +24,14 @@ function App() {
   function play(){
     audio.play()
     audio.autoplay = true;
-
   }
 
   
   return (
-    
       <div className={overallClass}> 
         <span className={showButton}><Button variant="primary" onClick={() => {startGame();play()}}>Start</Button></span>
         {showSussyBakas}
-        
-        
-    
       </div>
-      
-      
-    
   );
 }
 
--- src/Components/sussy-bakas.js
+++ src/Components/sussy-bakas.js
@@ -1,20 +1,20 @@
 import * as React from "react";
 import { Engine, RenderClones, Walls, Circle } from "react-matter-js";
 
+
 export function SussyBakas() {
     const width = window.innerWidth;
     const height = window.innerHeight+100;
     const circleCount = 125;
     const cirlceSize = height * 0.05;
-
-    const whoppyCount = 35;
-
+    const whoppyCount = 30;
+    
     const circleOptions = {
         render: {
             sprite:{
                 texture: "https://www.graphicpie.com/wp-content/uploads/2020/11/red-among-us-png-370x450.png",
                 xScale: 0.5,
-                yScale: 0.5
+                yScale: 0.5   
             }
             
         }
@@ -30,15 +30,16 @@ export function SussyBakas() {
             
         }
     }
-
   
     const renderCircles = () => {
+        
       return [...Array(circleCount).keys()].map(num => (
         <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={circleOptions}/>
       ));
     };
 
     const renderWhoppies = () => {
+        
         return [...Array(whoppyCount).keys()].map(num => (
           <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={whoppyOptions}/>
         ));
@@ -60,8 +61,9 @@ export function SussyBakas() {
             >
             <Walls x={0} y={0} width={width} height={height} wallWidth={10} />
             
-            {renderCircles()}
+            
             {renderWhoppies()}
+            {renderCircles()}
             
             </RenderClones>
         </Engine>
