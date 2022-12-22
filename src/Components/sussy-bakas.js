7f2e314c5427db1e9d77a2efa898e00ed7b10e1b
antzed <anthoz3@uw.edu>
2022-12-22 08:41:00 -0800

    sus goes abstract

--- src/Components/sussy-bakas.js
+++ src/Components/sussy-bakas.js
@@ -5,45 +5,34 @@ import { Engine, RenderClones, Walls, Circle } from "react-matter-js";
 export function SussyBakas() {
     const width = window.innerWidth;
     const height = window.innerHeight+100;
-    const circleCount = 125;
+    const circleCount = 100;
     const cirlceSize = height * 0.05;
     const whoppyCount = 30;
-    
-    const circleOptions = {
-        render: {
-            sprite:{
-                texture: "https://www.graphicpie.com/wp-content/uploads/2020/11/red-among-us-png-370x450.png",
-                xScale: 0.5,
-                yScale: 0.5   
+    const lemonSusCount = 30;
+
+    const susOptions = (url)   => {
+        return {
+            render: {
+                sprite:{
+                    texture: url,
+                    xScale: 0.5,
+                    yScale: 0.5
+                }
             }
-            
         }
     }
 
-    const whoppyOptions = {
-        render: {
-            sprite:{
-                texture: "https://www.graphicpie.com/wp-content/uploads/2020/11/among-us-green-png-370x450.png",
-                xScale: 0.5,
-                yScale: 0.5
-            }
-            
-        }
+    const susSriteUrl = {
+        circle: "https://www.graphicpie.com/wp-content/uploads/2020/11/red-among-us-png-370x450.png",
+        whoppy: "https://www.graphicpie.com/wp-content/uploads/2020/11/among-us-green-png-370x450.png",
+        lemonSus: "https://www.graphicpie.com/wp-content/uploads/2020/11/cyan-among-us-character-1-370x450.png"
     }
-  
-    const renderCircles = () => {
-        
-      return [...Array(circleCount).keys()].map(num => (
-        <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={circleOptions}/>
-      ));
-    };
 
-    const renderWhoppies = () => {
-        
-        return [...Array(whoppyCount).keys()].map(num => (
-          <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={whoppyOptions}/>
+    const renderSus = (count, options, url) => {
+        return [...Array(count).keys()].map(num => (
+            <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={options(url)}/>
         ));
-      };
+    }
 
     return (
         <>
@@ -60,10 +49,10 @@ export function SussyBakas() {
             }}
             >
             <Walls x={0} y={0} width={width} height={height} wallWidth={10} />
-            
-            
-            {renderWhoppies()}
-            {renderCircles()}
+             
+            {renderSus(circleCount, susOptions, susSriteUrl.circle)}
+            {renderSus(whoppyCount, susOptions, susSriteUrl.whoppy)}
+            {renderSus(lemonSusCount, susOptions, susSriteUrl.lemonSus)}
             
             </RenderClones>
         </Engine>
