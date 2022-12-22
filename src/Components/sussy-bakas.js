f085e5b06e4ca9147029cd5eec337350f8eae105
antzed <anthoz3@uw.edu>
2022-12-21 18:39:31 -0800

    the sus-queen demands whoppies to live among the sussy-bakas

--- src/Components/sussy-bakas.js
+++ src/Components/sussy-bakas.js
@@ -4,8 +4,10 @@ import { Engine, RenderClones, Walls, Circle } from "react-matter-js";
 export function SussyBakas() {
     const width = window.innerWidth;
     const height = window.innerHeight+100;
-    const circleCount = 150;
+    const circleCount = 125;
     const cirlceSize = height * 0.05;
+
+    const whoppyCount = 35;
 
     const circleOptions = {
         render: {
@@ -17,6 +19,18 @@ export function SussyBakas() {
             
         }
     }
+
+    const whoppyOptions = {
+        render: {
+            sprite:{
+                texture: "https://www.graphicpie.com/wp-content/uploads/2020/11/among-us-green-png-370x450.png",
+                xScale: 0.5,
+                yScale: 0.5
+            }
+            
+        }
+    }
+
   
     const renderCircles = () => {
       return [...Array(circleCount).keys()].map(num => (
@@ -24,10 +38,11 @@ export function SussyBakas() {
       ));
     };
 
-   
-
-    
-
+    const renderWhoppies = () => {
+        return [...Array(whoppyCount).keys()].map(num => (
+          <Circle key={num} clone x={300} y={100} radius={cirlceSize} options={whoppyOptions}/>
+        ));
+      };
 
     return (
         <>
@@ -44,7 +59,10 @@ export function SussyBakas() {
             }}
             >
             <Walls x={0} y={0} width={width} height={height} wallWidth={10} />
+            
             {renderCircles()}
+            {renderWhoppies()}
+            
             </RenderClones>
         </Engine>
         </>
